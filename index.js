b39820b3ef65d5a5fa47f4cc8a10815f498dbba9
Levi Nunnink <levi@droplr.com>
2018-05-01 09:23:25 -0700

    No check needed. Parsing will throw an error if base64 is not valid

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
 export const parseBase64ByPlattform = base64String => {
-  if (!base64String || !isValidBase64(base64String)) throw new Error('No valid base64 passed')
+  if (!base64String) throw new Error('No valid base64 passed')
 
   if (typeof window === 'undefined' && (process && process.version !== 'undefined')) {
     return Buffer.from(base64String, 'base64').toString()
   } else {
     return window.atob(base64String)
   }
-}
-
-export const isValidBase64 = base64String => {
-  if (!base64String) return false
-  if (!(/^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$/.test(base64String))) return false
-
-  return true
 }
 
 export const parseJwtToken = jwtToken => {
